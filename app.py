f209c9370cae48af9fc81f266d72f1b9f73a7aff
Harvin Zúniga <harvin.zuniga@gmail.com>
2020-11-13 21:27:12 +0000

    Creating routes for the aplication

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 import os
 from flask import (Flask, flash, render_template, redirect, request, session, url_for)
-from flask_pymongo import flask_pymongo
-from bson.objectId import objectId
-from werkseug.security import generate_password_hash, check_password_hash
-if os.path.exist("env.py"):
+if os.path.exists("env.py"):
     import env
 
 app = Flask(__name__)
@@ -12,11 +9,13 @@ app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
-mongo = PyMongo(app)
-
 
 @app.route("/")
-@app.route("/reviews")
 def get_reviews():
     return render_template("reviews.html")
 
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
